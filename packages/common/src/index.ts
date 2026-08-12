a564f002607c0059faf5cbb7a5fe520c3cec61c3
ruby <necrelox@proton.me>
2026-08-12 12:35:02 +0200

    feat(common): add create worker (is a factory to create worker with fn)

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,4 @@
+export { createWorker } from './create-worker';
 export { PARSE_HUMAN_TIME_ERROR_CODES, parseHumanTime } from './parse-human-time';
 export type { TimeUnit } from './parse-human-time';
 export { TypedEventEmitter } from './typed-event-emitter';
